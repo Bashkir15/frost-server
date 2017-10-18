518acad6de137137746a7365f552fe99442d8304
Forrest Collins <Forest.D.Collins@gmail.com>
2017-10-18 17:53:55 -0500

    Added dotenv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export { default as createCore } from './core';
 export { default as createError } from './error';
 export { default as createFallback } from './fallback';
